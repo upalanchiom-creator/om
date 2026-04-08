f411f7843c98d81761ebb8d5c9d969569ea5f91b
upalanchiom-creator <upalanchiom@gmail.com>
2026-04-08 12:16:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function toggleDarkMode() {
     document.body.classList.toggle("dark");
 }
+
+// Lightbox
+function openLightbox(img) {
+    document.getElementById("lightbox").style.display = "flex";
+    document.getElementById("lightbox-img").src = img.src;
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
